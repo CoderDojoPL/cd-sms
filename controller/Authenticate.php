a263e0dad00ce2600dd255e32b2bcbfada47d32b
DrafFter <bitbucket@draffter.net>
2015-07-14 00:32:12 +0200

    getting user information from Google API temporary eliminate token Google API time check

--- controller/Authenticate.php
+++ controller/Authenticate.php
@@ -1,85 +1,97 @@
 <?php
 
 namespace Controller;
+
 use Arbor\Core\Controller;
 use Arbor\Provider\Response;
 
-class Authenticate extends Controller{
+class Authenticate extends Controller
+{
 
-	public function index(){
+	public function index()
+	{
 	}
-	
-	public function login(){
+
+	public function login()
+	{
 	}
 
-	public function loginRedirect(){
-		$googleService=$this->getService('google');
+	public function loginRedirect()
+	{
+		$googleService = $this->getService('google');
 
-		$response=new Response();
+		$response = new Response();
 		$response->redirect($googleService->getClient()->createAuthUrl());
 		return $response;
 	}
 
-	public function loginOAuth2Callback(){
-		$googleService=$this->getService('google');
-		$client=$googleService->getClient();
-
-		$session=$this->getRequest()->getSession();
-
-		$query=$this->getRequest()->getQuery();
+	public function loginOAuth2Callback()
+	{
+		$googleService = $this->getService('google');
+		$client = $googleService->getClient();
+		/* @var $client \Google_Client */
+		$session = $this->getRequest()->getSession();
+		$query = $this->getRequest()->getQuery();
 		if (isset($query['code'])) {
-		  $client->authenticate($query['code']);
-		  $session->set('access.token',$client->getAccessToken());
+			$client->authenticate($query['code']);
+			$session->set('access.token', $client->getAccessToken());
 
-		  $tokenData = $client->verifyIdToken()->getAttributes();
-		  $email=$tokenData['payload']['email'];
-		  if(!preg_match('/^.*?@coderdojo.org.pl$/',$email)){
-		  	$session->clear();
-		  }
-		  else{
+			$tokenData = $client->verifyIdToken()->getAttributes();
+			$userInfoData = $googleService->getAddInfo()->userinfo->get();
+			/* @var $userInfoData \Google_Service_Oauth2_Userinfoplus */
+			$email = $tokenData['payload']['email'];
+			if (!preg_match('/^.*?@coderdojo.org.pl$/', $email)) {
+				$session->clear();
+			} else {
 
-		  	$user=$this->findOne('User',array('email'=>$email));
-		  	if(!$user){		  		
-		  		$user=$this->createUser($tokenData['payload']);
-		  	}
+				$user = $this->findOne('User', array('email' => $email));
+				if (!$user) {
+					$user = $this->createUser($tokenData['payload'], $userInfoData);
+				}
 
-		  	$session->set('user.id',$user->getId());
+				$session->set('user.id', $user->getId());
 
-		  }
+			}
 
 		}
 
-		$response=new Response();
+		$response = new Response();
 		$response->redirect('/');
 		return $response;
-	
+
 	}
 
-	public function logout(){
-		$googleService=$this->getService('google');
-		$client=$googleService->getClient();
+	public function logout()
+	{
+		$googleService = $this->getService('google');
+		$client = $googleService->getClient();
 
 		$client->revokeToken();
 		$this->getRequest()->getSession()->clear();
 
-		$response=new Response();
+		$response = new Response();
 		$response->redirect('/');
 		return $response;
-		
+
 	}
 
-	private function createUser($data){
-		$userEntity=new \Entity\User();
+	/**
+	 * @param $data
+	 * @param \Google_Service_Oauth2_Userinfoplus $userData
+	 * @return \Entity\User
+	 */
+	private function createUser($data, $userData)
+	{
+		$userEntity = new \Entity\User();
 		$userEntity->setEmail($data['email']);
-		$userEntity->setFirstName('TODO');
-		$userEntity->setLastName('TODO');
-	
+		$userEntity->setFirstName($userData['givenName']);
+		$userEntity->setLastName($userData['familyName']);
+
 		$this->persist($userEntity);
 		$this->flush();
 
 		return $userEntity;
 	}
-
 
 
 }
--- library/google/service/Google.php
+++ library/google/service/Google.php
@@ -12,27 +12,42 @@ use Arbor\Core\EventManager;
 
 require_once '../library/google/engine/src/Google/autoload.php';
 
-class Google{
-	
-	public function __construct(ServiceConfig $serviceConfig,EventManager $eventManager){
+class Google
+{
+
+	private $addInfo;
+
+	public function __construct(ServiceConfig $serviceConfig, EventManager $eventManager)
+	{
 
 		$client_id = $serviceConfig->get('clientId');
-		 $client_secret = $serviceConfig->get('clientSecret');
-		 $redirect_uri = $serviceConfig->get('redirectUri');
+		$client_secret = $serviceConfig->get('clientSecret');
+		$redirect_uri = $serviceConfig->get('redirectUri');
 		$client = new \Google_Client();
 		$client->setClientId($client_id);
 		$client->setClientSecret($client_secret);
 		$client->setRedirectUri($redirect_uri);
-		$client->addScope("email profile");
-		$this->client=$client;
+		$client->setScopes(array('https://www.googleapis.com/auth/userinfo.email','https://www.googleapis.com/auth/userinfo.profile'));
+		$this->addInfo = new \Google_Service_Oauth2($client);
+		$this->client = $client;
 	}
 
-	public function isAuthenticated(){
+	public function isAuthenticated()
+	{
 		return isset($_SESSION['access_token']);
 	}
 
-	public function getClient(){
+	public function getClient()
+	{
 		return $this->client;
+	}
+
+	/**
+	 * @return \Google_Service_Oauth2
+	 */
+	public function getAddInfo()
+	{
+		return $this->addInfo;
 	}
 
 }
