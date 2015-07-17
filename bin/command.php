7cb39844a188359cd5a37000146c24e63638da24
Michał Tomczak <michal.tomczak@newaxis.pl>
2015-07-17 20:08:41 +0200

    Change enviorment for arbor command

--- bin/command.php
+++ bin/command.php
@@ -3,5 +3,11 @@
 require_once __DIR__.'/../arbor/Root.php';
 
 array_shift($argv);
-$root=new Arbor\Root(true,true,'dev');
+$env='prod';
+$debug=false;
+if(file_exists('../dev')){
+	$env='dev';
+	$debug=true;
+}
+$root=new Arbor\Root($debug,true,$env);
 $root->executeCommand($argv);
