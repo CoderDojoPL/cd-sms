883e1cc49e515da3078a84a45940368fc7cb898b
DrafFter <github@draffter.net>
2016-04-12 20:55:09 +0200

    missing migrate

---
+++ migrate/Version20160412200400.php
@@ -0,0 +1,42 @@
+<?php
+namespace Migrate;
+
+use Arbor\Core\Container;
+use Common\MigrateHelper;
+
+/**
+ * Created by PhpStorm.
+ * User: DrafFter
+ * Date: 2016-04-12
+ * Time: 20:04
+ */
+
+class Version20160412200400 extends MigrateHelper
+{
+	public function update(Container $container)
+	{
+		$this->beginTransaction();
+		$schema = $this->createSchema();
+
+		$queueMail= $schema->createTable('queue_emails');
+		$queueMail->addColumn('id','integer',array('autoincrement'=>true));
+		$queueMail->addColumn('to','string');
+		$queueMail->addColumn('subject','string');
+		$queueMail->addColumn('content','text');
+		$queueMail->addColumn('sended_at','datetime',array('notnull'=>false));
+		$queueMail->addColumn('created_at','datetime',array('notnull'=>false));
+		$queueMail->setPrimaryKey(array('id'));
+		$this->updateSchema($schema);
+		$this->commitTransaction();
+	}
+
+	public function downgrade(Container $container)
+	{
+		$this->beginTransaction();
+		$schema = $this->createSchema();
+		$schema->dropTable('queue_emails');
+		$this->updateSchema($schema);
+
+		$this->commitTransaction();
+	}
+}
